5d6281dd16844da9b4f9f0645b73f7dddab5abdc
Peter Kester <peetkes+github@gmail.com>
2016-11-02 12:34:08 +0100

    added request params to post and multipart post

--- src/main/java/com/marklogic/rest/RestPoster.java
+++ src/main/java/com/marklogic/rest/RestPoster.java
@@ -2,6 +2,7 @@ package com.marklogic.rest;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -17,6 +18,7 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -53,7 +55,7 @@ public class RestPoster {
     private static int mlPort = 8004;
     private static String mlUser = "admin";
     private static String mlPasswrd = "admin";
-    private static String mlEndpoint = "/v1/documents";
+    private static String mlEndpoint = "/v1/resources/ingest-multipart";
     private final CloseableHttpClient httpSrc;
 
     public RestPoster() {
@@ -70,26 +72,17 @@ public class RestPoster {
 
     }
 
-    public int post(List<MLPayLoad> payLoads, Date date, String[] collections) throws URISyntaxException, IOException {
+    public int post(List<NameValuePair> requestParams, List<MLPayLoad> payLoads, Date date, String[] collections) throws URISyntaxException, IOException {
         URI uriSrc = new URIBuilder()
                 .setScheme("http")
                 .setHost(mlHostName)
                 .setPort(mlPort)
                 .setPath(mlEndpoint)
+                .setParameters(requestParams)
                 .build();
-        JSONArray colls = new JSONArray();
-        for (String collection : collections) {
-            colls.add(collection);
-        }
-        JSONObject obj = new JSONObject();
-        obj.put("collections",colls);
-        String jsonCollection = JSONValue.toJSONString(obj);
-        ContentBody inlineColls = new MLStringBody(jsonCollection, ContentType.create("text/json", "UTF-8"),null);
-
         List<ContentBody> contentBodies = new ArrayList<ContentBody>();
 
         for (MLPayLoad payLoad : payLoads) {
-//			ContentType contentType = ContentType.create("text/json", "UTF-8");
             if (payLoad.hasMsg()) {
                 contentBodies.add(new MLStringBody(payLoad.getMsg(), payLoad.getContentType(), payLoad.getUri()));
             } else {
@@ -100,7 +93,6 @@ public class RestPoster {
             return 0;
         } else {
             HttpEntity reqEntity = MixedMultipartEntityBuilder.create()
-                    .addPart("inline; category=metadata", inlineColls)
                     .addParts("attachment",contentBodies)
                     .build();
             HttpPost postRequest = new HttpPost(uriSrc);
@@ -114,12 +106,13 @@ public class RestPoster {
             return response.getStatusLine().getStatusCode();
         }
     }
-    public int post(String uri, String msg, Date date) throws URISyntaxException, SAXException, ParserConfigurationException, IOException, TransformerException {
+    public int post(String uri, List<NameValuePair> requestParams, String msg, Date date) throws URISyntaxException, SAXException, ParserConfigurationException, IOException, TransformerException {
         URI uriSrc = new URIBuilder()
                 .setScheme("http")
                 .setHost(mlHostName)
                 .setPort(mlPort)
                 .setPath(mlEndpoint)
+                .setParameters(requestParams)
                 .build();
         StringBody strBody = new MLStringBody(msg, ContentType.create("application/xml", "UTF-8"), uri);
         HttpEntity reqEntity = MixedMultipartEntityBuilder.create()
@@ -167,8 +160,12 @@ public class RestPoster {
         log.info("Starting application RestPoster");
         RestPoster client = new RestPoster();
         try {
-            String test = "<sv:MSG xmlns:sv=\"http://www.testcom/DA/CF_Main\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><content xmlns=\"\">Dit is content</content></sv:MSG>";
-            client.post("/content/100100100.xml", test, new Date());
+            String uri = "/content/100100100.xml";
+            List<NameValuePair> requestParams = new ArrayList<NameValuePair>();
+            NameValuePair source = new BasicNameValuePair("rs:source","BCDB");
+            requestParams.add(source);
+            String payload = "<sv:MSG xmlns:sv=\"http://www.testcom/DA/CF_Main\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><content xmlns=\"\">Dit is content</content></sv:MSG>";
+            client.post(uri, requestParams, payload, new Date());
         } catch (URISyntaxException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
